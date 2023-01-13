fe1d190d2263be67eb26a102e923157f106006ad
sixtosMateo <sixtosmateo@gmail.com>
2023-01-12 20:49:09 -0800

    routing concept in flask

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from markupsafe import escape
+
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def hello_world():
-    return f"Hello, {escape(name)}!"
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
