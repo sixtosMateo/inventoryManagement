a2bbf2dc43c76c91f26c1aecf807aa5fe94a4aea
sixtosMateo <sixtosmateo@gmail.com>
2023-01-12 20:42:46 -0800

    any user-provided values rendered in the output must be escaped to protect from injection attacks

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/<name>")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return f"Hello, {escape(name)}!"
