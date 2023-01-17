2aae4bb41bc89afc9dd91790c40fb51e3b67b405
sixtosMateo <sixtosmateo@gmail.com>
2023-01-16 23:31:36 -0800

    Capturing data from user input

--- app.py
+++ app.py
@@ -79,7 +79,25 @@ with app.test_request_context('/hello', method='POST'):
     print('Stating unit testing for request context')
     assert request.path == '/hello'
     assert request.method == 'POST'
+    print('ending untit testing for request context')
 
 '''Passing a whole environment'''
 with app.request_context(environ):
     assert request.method == 'POST'
+
+'''accepting data from form'''
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+'''Accessing url parameters not user friendly'''
+searchword = request.args.get('key', '')
