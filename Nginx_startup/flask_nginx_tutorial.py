011b0b03b0e66536dc38eb3cd4ee64ee73db7610
sixtosMateo <sixtosmateo@gmail.com>
2023-01-26 20:08:43 -0800

    Nginx tutorial part 1

---
+++ Nginx_startup/flask_nginx_tutorial.py
@@ -0,0 +1,17 @@
+'''
+NGINX
+
+- nginx is a fast, production level HTTP level
+- when serving your application with one of the WSGI servers it's good or necessary
+to put a dedicated HTTP server in front of it
+- This 'reverse proxy' can handle incoming requests, TLS and other security and
+performance concerns better than the WSGI server
+
+Installing and running Nginx itself is outside the scope of this tutorial
+
+- this tutorial outlines the basics of configuring NGINX to proxy your application
+
+Domain Name
+
+
+'''
