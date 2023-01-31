1186b5170ec211180aa00b860d63496d11cd03cd
sixtosMateo <sixtosmateo@gmail.com>
2023-01-30 23:30:42 -0800

    Project Flaskr Layout

---
+++ flask-tutorial/Flaskr/blog.py
@@ -0,0 +1,8 @@
+'''
+Flask Tutorial App: Flaskr
+Created By: Mateo Sixtos
+Date: 1/30/23
+Description: Blog application. Users are able to register, log in, create post,
+edit or delete their own post. This application will be able to be package and
+can be install on other computers.
+'''
