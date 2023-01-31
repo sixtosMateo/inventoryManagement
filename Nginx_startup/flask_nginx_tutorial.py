b580d764cd893a7a4c31490d722569482c4cb2ed
sixtosMateo <sixtosmateo@gmail.com>
2023-01-30 23:04:18 -0800

    X-Forwarded headers and proxy fix notes

--- Nginx_startup/flask_nginx_tutorial.py
+++ Nginx_startup/flask_nginx_tutorial.py
@@ -59,5 +59,40 @@ Then tell Flask it is behind a Proxy so that your application uses these headers
 '''
 How do you tell Flask that it is behind a proxy ?
 
+When using a proxy or many Python hosting platforms the proxy will intercept and
+forward all external requests to the local wsgi server
 
+From the WSGI server and Flask application's perspectives requests are now coming
+from the HTTP server to the local address,
+    Rather than from the remote address to the external server address
+
+
+X-Forwarded headers
+
+HTTP servers should set X-Forwarded headers to pass on the real values to the
+application
+    - The application can then be told to trust and use those values by wrapping
+    it with the X-Forwarded-For Proxy Fix middleware provided by Werkzeug
+
+
+X-Forwarded-For Proxy Fix middleware
+
+This middleware should only be used if the applicataion is actually behind a proxy
+and should be configured with the number of proxies that are chained in front of
+it
+    - Not all proxies set all the headers
+    - Since incoming headers can be fake you must set how many proxies are setting
+    each header so that middleware know what to trust
+
+************************************************************************
+    from werkzeug.middleware.proxy_fix import ProxyFix
+
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+        )
+************************************************************************
+
+Only apply this middleware if you are behind a proxy and set the correct number
+of proxies that set each header
+    - It can be a security issue if you get this configuration wrong
 '''
