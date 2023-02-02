2de48392e3dcc5e8030220795322c0cb6537d47f
sixtosMateo <sixtosmateo@gmail.com>
2023-02-02 00:13:42 -0800

    Explanation of connecting to db code

--- flask-tutorial/Flaskr/db.py
+++ flask-tutorial/Flaskr/db.py
@@ -1,5 +1,32 @@
 import sqlite3, click
 from flask import current_app, g
+'''
+g is an object that is unique for each request
+    - it is used to store data that might be accessed by multiple functions
+    during the request
+    - the connection is stored and reused instead of creating a new connection
+    if get_db is called a second time in the same request
+
+current_app is an object that points to the Flask application handling the request
+    - since you used an application factory there is no application object when
+    writing the rest of your code
+    - get_db will be called when the application has been created and is handling
+    a request so current_app can be used
+
+sqlite3.connect() establishes a connection to the file pointed at by the DATABASE
+configuration key
+    - This file doesnt have to exist yet and wont until you initialize the datase
+    later
+
+sqlite3.Row tells the connection to return rows that behave like dicts.
+    - This allows accessing the column by name
+
+close_db checks if a connection was created by checking if g.db was set.
+    - If the connection exists, it is closed.
+    - Further down you will tell your application about the close_db function in
+     the application factory so that it is called after each request.
+'''
+
 
 def get_db():
     if 'db' not in g:
@@ -7,6 +34,7 @@ def get_db():
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
+
         g.db.row_factory = sqlite3.Row
 
     return g.db
