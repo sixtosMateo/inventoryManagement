d7db6990b39f77e1bbffe0a4483048cc6ed933e6
sixtosMateo <sixtosmateo@gmail.com>
2023-02-02 23:38:20 -0800

    Initialize db with schema file + command line that calls init db method

--- flask-tutorial/Flaskr/db.py
+++ flask-tutorial/Flaskr/db.py
@@ -26,6 +26,22 @@ close_db checks if a connection was created by checking if g.db was set.
     - Further down you will tell your application about the close_db function in
      the application factory so that it is called after each request.
 '''
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+'''
+click.command(): defines a command line command called init-db that calls the
+init_db function and shows a success message to the user. You can read
+Command Line Interface to learn more about writing commands.
+'''
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
 
 
 def get_db():
