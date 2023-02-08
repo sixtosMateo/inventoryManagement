bd22ae478844b49878daab556871e45d6f284f60
sixtosMateo <sixtosmateo@gmail.com>
2023-02-07 23:27:20 -0800

    View function: Post registration code

--- flask-tutorial/Flaskr/auth.py
+++ flask-tutorial/Flaskr/auth.py
@@ -6,4 +6,37 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
+'''__name__ arg
+    - Like the application object, the blueprint needs to know where it is
+    defined so __name__ is passed as the second argument
+'''
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
