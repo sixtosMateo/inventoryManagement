7536c6b075972cd0ddafc7e527b46a7bb4282af9
sixtosMateo <sixtosmateo@gmail.com>
2023-02-10 00:21:49 -0800

    decorator to check user is logged in to do CRUD operations

--- flask-tutorial/Flaskr/auth.py
+++ flask-tutorial/Flaskr/auth.py
@@ -120,3 +120,13 @@ load_logged_in_user function to load a user info on subsequent requests
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
