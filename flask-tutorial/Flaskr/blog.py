5d4f5229f7585d1f4ebbbfe5f0d827d623ff9f50
sixtosMateo <sixtosmateo@gmail.com>
2023-02-13 23:43:49 -0800

    initialize blog blueprint and register it to create app: init.py

--- flask-tutorial/Flaskr/blog.py
+++ flask-tutorial/Flaskr/blog.py
@@ -6,3 +6,13 @@ Description: Blog application. Users are able to register, log in, create post,
 edit or delete their own post. This application will be able to be package and
 can be install on other computers.
 '''
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
