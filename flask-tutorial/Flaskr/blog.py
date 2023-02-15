0491ef18af80773eb831a7fcc1ccc6a5eb017e48
sixtosMateo <sixtosmateo@gmail.com>
2023-02-15 00:01:54 -0800

    Update view and get_post view with comments

--- flask-tutorial/Flaskr/blog.py
+++ flask-tutorial/Flaskr/blog.py
@@ -53,3 +53,71 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    '''
+    abort will raise a special exception that returns an HTTP status code
+        - It takes an optional message to show with the error, otherwise a
+        default message is used
+
+    check_author argument is defined so that the function can be used to get a
+    post without checking the author
+        - would be useful if you wrote a view to show a individual post on a
+        page, where the user doesnt matter because they are not modifying the
+        post
+
+
+    '''
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    '''
+    the update function takes an argument, id
+        - that id corresponds to the <int:id> in the route
+        - if you dont specify int: and instead do <id> it will be treated as a
+        string
+
+    To generate a url to the update page, url_for() needs to be passed the id
+    so it knows what to fill in:
+        url_for('blog.update', id=post['id'])
+    '''
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
